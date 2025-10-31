9437a508e2b5a0ca9abff6ea387a5eb76682c14d
Bilal <bk.freetime1122@gmail.com>
2025-10-31 21:48:42 +0100

    neuerung

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hallo");
         System.out.println("Hallo2");
+        System.out.println("haaaaaa");
     }
 }
